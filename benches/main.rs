1c63edc7fee7cbde9ba5a434e7bd94d01b674401
Ryuta Hayashi <ryuta.hayashi@fujifilm.com>
2020-07-05 17:38:46 +0900

    bench関数を一か所にまとめ

--- benches/main.rs
+++ benches/main.rs
@@ -4,6 +4,7 @@ extern crate criterion;
 use criterion::Criterion;
 use criterion::black_box;
 
+/*
 fn format_url_vec_concat(c: &mut Criterion) {
     let index = "test_idx".to_string();
     let name = "test_alias".to_string();
@@ -64,5 +65,96 @@ fn format_url_push(c: &mut Criterion) {
         }));
 }
 
-criterion_group!(benches, format_url_array_concat, format_url_vec_concat, format_url_macro, format_url_concat, format_url_push);
+fn format_url_push_no_capacity(c: &mut Criterion) {
+    let index = "test_idx".to_string();
+    let name = "test_alias".to_string();
+
+    c.bench_function("format_url_push_no_capacity",
+        |b| b.iter(|| {
+            let mut url = String::new();
+            url.push_str("/");
+            url.push_str(&index);
+            url.push_str("/_alias/");
+            url.push_str(&name);
+            url
+        }));
+}
+
+fn format_url_array_concat_str(c: &mut Criterion) {
+    let index = "test_idx";
+    let name = "test_alias";
+
+    c.bench_function("format_url_array_concat_str",
+        |b| b.iter(|| {
+            let url = [
+                "/", index, "/_alias/", name
+            ].concat();
+            url
+        }));
+}
+*/
+
+fn format_url_benches(c: &mut Criterion) {
+    let index = "test_idx";
+    let name = "test_alias";
+
+    c.bench_function("format_url_vec_concat",
+        |b| b.iter(|| {
+            let url = vec!["/", &index[..], "/_alias/", &name[..]].concat();
+            url
+        }));
+
+    c.bench_function("format_url_array_concat",
+        |b| b.iter(|| {
+            let url = ["/", &index[..], "/_alias/", &name[..]].concat();
+            url
+        }));
+
+    c.bench_function("format_url_macro",
+        |b| b.iter(|| {
+            format!("/{}/_alias/{}", index, name);
+        }));
+
+    c.bench_function("format_url_concat",
+        |b| b.iter(|| {
+            let mut url = "/".to_string();
+            url = url + &index[..] + "/_alias/" + &name[..];
+            url
+        }));
+
+    c.bench_function("format_url_push",
+        |b| b.iter(|| {
+            let mut url = String::with_capacity(1 + "/_alias/".len()
+                + index.len() + name.len());
+            url.push_str("/");
+            url.push_str(&index);
+            url.push_str("/_alias/");
+            url.push_str(&name);
+            url
+        }));
+
+    c.bench_function("format_url_push_no_capacity",
+        |b| b.iter(|| {
+            let mut url = String::new();
+            url.push_str("/");
+            url.push_str(&index);
+            url.push_str("/_alias/");
+            url.push_str(&name);
+            url
+        }));
+
+    c.bench_function("format_url_array_concat_str",
+        |b| b.iter(|| {
+            let url = [
+                "/", index, "/_alias/", name
+            ].concat();
+            url
+        }));
+
+}
+
+criterion_group!(benches, format_url_benches);
+    //format_url_array_concat, format_url_vec_concat, format_url_macro,
+    //format_url_concat, format_url_push, format_url_push_no_capacity,
+    //format_url_array_concat_str);
 criterion_main!(benches);
